b324d4b7b9f6133cfae4982ddcef2f7145b8cdd7
sarith.vasu <sarithvasu@gmail.com>
2017-04-18 14:51:21 +0530

    18-04-2017_02_51

--- app/src/main/java/com/effone/hostismeandroid/adapter/MenuItemSummeryListAdapter.java
+++ app/src/main/java/com/effone/hostismeandroid/adapter/MenuItemSummeryListAdapter.java
@@ -22,11 +22,11 @@ import java.util.List;
  */
 
 public class MenuItemSummeryListAdapter extends ArrayAdapter<OrderedItemSummary> {
-    private ArrayList<OrderedItemSummary> restaurants;
+    private ArrayList<OrderedItemSummary> orderedItemSummaries;
     private LayoutInflater inflater;
-    public MenuItemSummeryListAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<OrderedItemSummary> restaurants) {
-        super(context, resource,  restaurants);
-        this.restaurants =(ArrayList<OrderedItemSummary>) restaurants;
+    public MenuItemSummeryListAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<OrderedItemSummary> orderedItemSummaries) {
+        super(context, resource,  orderedItemSummaries);
+        this.orderedItemSummaries =(ArrayList<OrderedItemSummary>) orderedItemSummaries;
         inflater = (LayoutInflater)context.
                 getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
@@ -34,38 +34,46 @@ public class MenuItemSummeryListAdapter extends ArrayAdapter<OrderedItemSummary>
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View vi = convertView;
-        final RestaurantListAdapter.FilterViewHolder holder;
+        final MenuItemSummeryListAdapter.FilterViewHolder holder;
 
         if(convertView==null){
-            vi = inflater.inflate(R.layout.restaurant_list_item, null);
-            holder = new RestaurantListAdapter.FilterViewHolder();
-            holder.tv_restaurant_name=(TextView)vi.findViewById(R.id.tv_restaurant_name);
-            holder.tv_restaurant_adress = (TextView) vi.findViewById(R.id.tv_restaurant_adress);
+            vi = inflater.inflate(R.layout.summery_list_item, null);
+            holder = new MenuItemSummeryListAdapter.FilterViewHolder();
+            holder.tv_summery_item_name=(TextView)vi.findViewById(R.id.tv_summery_item_name);
+            holder.tv_item_total_price = (TextView) vi.findViewById(R.id.tv_item_total_price);
+            holder.tv_service_charges_value=(TextView)vi.findViewById(R.id.tv_service_charges_value);
+            holder.tv_vat_value = (TextView) vi.findViewById(R.id.tv_vat_value);
+            holder.tv_service_tax_value=(TextView)vi.findViewById(R.id.tv_service_tax_value);
+            holder.tv_total = (TextView) vi.findViewById(R.id.tv_total);
             vi.setTag( holder );
         }
         else
-            holder = (RestaurantListAdapter.FilterViewHolder) vi.getTag();
+            holder = (MenuItemSummeryListAdapter.FilterViewHolder) vi.getTag();
 
-        if (restaurants.size() <= 0) {
-            holder.tv_restaurant_name.setText("No Data");
+        if (orderedItemSummaries.size() <= 0) {
+            holder.tv_summery_item_name.setText("No Data");
 
         } else {
             /***** Get each Model object from Arraylist ********/
-            OrderedItemSummary value = (OrderedItemSummary) restaurants.get(position);
+            OrderedItemSummary value = (OrderedItemSummary) orderedItemSummaries.get(position);
             /************  Set Model values in Holder elements ***********/
-            holder.tv_restaurant_name.setText(value.getItemName());
-           // holder.tv_restaurant_adress.setText(value.getRestAdress()+", "+value.getCity()+", "+value.getCountry());
+            holder.tv_summery_item_name.setText(value.getItemName()+"("+value.getQuatity()+")");
+            holder.tv_item_total_price.setText("$ "+value.getPrice());
+            holder.tv_service_charges_value.setText("$ "+value.getServiceCharges());
+            holder.tv_vat_value.setText("$ "+value.getVat());
+            holder.tv_service_tax_value.setText("$ "+value.getServiceTax());
         }
 
         return vi;
 
     }
     public static  class FilterViewHolder {
-        TextView tv_restaurant_name;
-        TextView tv_restaurant_adress;
-
-
-
+        TextView tv_summery_item_name;
+        TextView tv_item_total_price;
+        TextView tv_service_charges_value;
+        TextView tv_vat_value;
+        TextView tv_service_tax_value;
+        TextView tv_total;
     }
 }
 
